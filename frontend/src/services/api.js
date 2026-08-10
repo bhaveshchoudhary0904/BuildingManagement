2fdc0b001d5d05c091f774d45a6ddb4f875f7e56
bhavesh <bhaveshchoudhary0904@gmail.com>
2026-08-10 15:19:41 +0530

    Connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://building-management-chi.vercel.app/api",
 });
 
 export default api;
